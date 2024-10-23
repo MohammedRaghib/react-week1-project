3407c28e0678aa31035a158f4b41a0dd494e55ee
MohammedRaghib <mohammedmraghib@gmail.com>
2024-10-23 12:35:48 +0300

    Made it to display with props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Main from './Components/Main';
+import Hobbit from './hobbit.jpg';
+import Percy from './percy_jackson.jpg';
 
 function App() {
+  const books = [
+    {
+      img: Percy,
+      title: 'Percy Jackson - The Last Olympian',
+      description: `A brave young boy setting out to destroy the now awakened Lord of the Titans: Kronos.`,
+    },
+    {
+      img: Hobbit,
+      title: 'The Hobbit',
+      description: `A book about a hobbit.`,
+    },
+  ];
+
   return (
     <div className="App">
-      <Header/>
-      <Main/>
-      <Footer/>
+      <Header />
+      {books.map((book, index) => (
+        <Main key={index} book={book} />
+      ))}
+      <Footer />
     </div>
   );
 }
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,36 +1,15 @@
-import React from 'react'
-import './card.css';
-import Hobbit from './hobbit.jpg';
-import Percy from'./percy_jackson.jpg';
+import React from 'react';
 
-function Main() {
-    const books = [
-        {
-            img: Percy,
-            title: 'Percy Jackson - The last olympian',
-            description: `A brave young boy setting out to destroy the now awakened Lord of the titans: Kronos.`
-        },
-        {
-            img: Hobbit,
-            title: 'The Hobbit',
-            description: `A book about hobbit`
-        }
-    ];
+function Main({ book }) {  // Destructure book from props
     return (
-        <>
-            <div className='cards'>
-                {books.map((book, index) => (
-                    <div key={index} className="book-card">
-                        <img src={book.img} alt={book.title}/>
-                        <div className="book-card-content">
-                            <h2 className="book-title">{book.title}</h2>
-                            <p className="book-description">{book.description}</p>
-                        </div>
-                    </div>
-                ))}
+        <div className="book-card">
+            <img src={book.img} alt={book.title} />
+            <div className="book-card-content">
+                <h2 className="book-title">{book.title}</h2>
+                <p className="book-description">{book.description}</p>
             </div>
-        </>
-    )
+        </div>
+    );
 }
 
-export default Main
+export default Main;
