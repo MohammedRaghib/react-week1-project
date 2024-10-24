d66fdf783d0e8f144561623e8e6a98671231fb20
MohammedRaghib <mohammedmraghib@gmail.com>
2024-10-24 10:20:16 +0300

    Increased number of books

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import Footer from './Components/Footer';
 import Main from './Components/Main';
 import Hobbit from './hobbit.jpg';
 import Percy from './percy_jackson.jpg';
+import Harry_Potter from './Harry_Potter.jpg';
+import Lord_of_the_Rings from './Lord_of_the_Rings.jpg';
+import Chronicles_of_Narnia from './Chronicles_of_Narnia.jpg';
+import Hunger_Games from './Hunger_Games.jpg';
+import Divergent from './Divergent.jpg';
 
 function App() {
   const books = [
@@ -17,6 +22,31 @@ function App() {
       title: 'The Hobbit',
       description: `A book about a hobbit.`,
     },
+    {
+      img: Harry_Potter,
+      title: 'Harry Potter and the Sorcerers Stone',
+      description: 'A young wizard discovers he is the chosen one to defeat the evil Lord Voldemort.',
+    },
+    {
+      img: Lord_of_the_Rings,
+      title: 'The Lord of the Rings',
+      description: 'A fellowship of hobbits, elves, dwarves, men, and wizards set out to destroy the One Ring and defeat the Dark Lord Sauron.',
+    },
+    {
+      img: Chronicles_of_Narnia,
+      title: 'The Chronicles of Narnia',
+      description: 'A series of fantasy novels about children who discover a magical land through a wardrobe, a painting, and a wardrobe.',
+    },
+    {
+      img: Hunger_Games,
+      title: 'The Hunger Games',
+      description: 'A dystopian novel about a girl who volunteers to take her younger sisters place in a deadly televised competition.',
+    },
+    {
+      img: Divergent,
+      title: 'Divergent',
+      description: 'A dystopian novel about a girl who doesnt fit into any of the five factions in her society.',
+    }
   ];
 
   return (
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function Main({ book }) {  // Destructure book from props
+function Main({ book }) {
     return (
         <div className="book-card">
-            <img src={book.img} alt={book.title} />
+            <img src={book.img} alt={book.title} height={276}/>
             <div className="book-card-content">
                 <h2 className="book-title">{book.title}</h2>
                 <p className="book-description">{book.description}</p>
